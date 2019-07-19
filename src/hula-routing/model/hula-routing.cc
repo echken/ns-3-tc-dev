74677c182130eacbe266efed796730db6fe53f43
echken <305020597@qq.com>
2019-07-19 10:03:47 +0800

    0718, error rebase! fixup 0719

--- src/hula-routing/model/hula-routing.cc
+++ src/hula-routing/model/hula-routing.cc
@@ -4,8 +4,76 @@
 
 namespace ns3 {
 
-/* ... */
+TypeId Ipv4HulaRouting::GetTypeId()
+{
+  static TypeId tid = TypeId("ns3::Ipv4HulaRouting")
+    .SetParent<Object>()
+    .SetGroupName("hula-routing")
+    .AddConstructor<Ipv4HulaRouting>()
+    .AddAttribute("HulaPathUtilUpdateInterval",
+                  "set hula probe frenquency",
+                  TimeValue(MicroSeconds(200)),
+                  MakeTimeAccessor(&Ipv4HulaRouting::m_pathUpdateIterval),
+                  MakeTimeChecker());
 
+  return tid;
+}
+
+void Ipv4HulaRouting::Ipv4HulaRouting():
+  m_ipv4(0),
+  m_flowletTimeOut(MicroSeconds(500)),
+  m_pathUpdateIterval(MicroSeconds(100)),
+  m_latestPathUtilUpdateTime(0)
+{
 
 }
 
+void Ipv4HulaRouting::~Ipv4HulaRouting()
+{
+
+}
+
+TypeId Ipv4HulaRouting::GetInstanceTypeId() const
+{
+  return Ipv4HulaRouting::GetTypeId();
+}
+
+
+void
+Ipv4CongaRouting::NotifyInterfaceUp (uint32_t interface)
+{
+}
+
+void
+Ipv4CongaRouting::NotifyInterfaceDown (uint32_t interface)
+{
+}
+
+void
+Ipv4CongaRouting::NotifyAddAddress (uint32_t interface, Ipv4InterfaceAddress address)
+{
+}
+
+void
+Ipv4CongaRouting::NotifyRemoveAddress (uint32_t interface, Ipv4InterfaceAddress address)
+{
+}
+
+void
+Ipv4CongaRouting::PrintRoutingTable (Ptr<OutputStreamWrapper> stream, Time::Unit uint) const 
+{
+}
+
+void Ipv4HulaRouting::SetIpv4(Ptr<Ipv4> ipv4)
+{
+  m_ipv4 = ipv4;
+}
+
+Ptr<Ipv4Route> Ipv4HulaRouting::RouteOutput(Ptr<Packet> p, const Ipv4Header &header, Ptr<NetDevice> oif, Socket::SocketErrno &socketError)
+{
+  NS_LOG_ERROR(this << "l3 routing protocol, can't route to upper layer");
+  return 0;
+}
+
+bool Ipv4HulaRouting::RouteInput(Ptr<const Packet> p, const Ipv4Header &header, Ptr<NetDevice> idev,
+                                 uni)
--- src/hula-routing/model/hula-routing.h
+++ src/hula-routing/model/hula-routing.h
@@ -5,6 +5,7 @@
 #include "ns3/ipv4-routing-protocol.h"
 #include "ns3/object.h"
 #include "ns3/ipv4.h"
+#include "ns3/nstime.h"
 
 #include <algorithm>
 #include <vector>
@@ -16,25 +17,56 @@ namespace ns3 {
 class Ipv4HulaRouting : public Ipv4RoutingProtocol {
 public:
   static TypeId GetTypeId(void);
-  virtual TypeId GetInstanceTypeId(void);
   Ipv4HulaRouting();
   ~Ipv4HulaRouting();
+  virtual TypeId GetInstanceTypeId(void);
 
-  //processing probe packet: 
+  //inherit from base 
+  virtual void NotifyInterfaceUp(uint32_t interface);
+  virtual void NotifyInterfaceDown(uint32_t interface);
+  virtual void NotifyAddAddress(uint32_t interface, Ipv4Address address);
+  virtual void NotifyRemoveAddress(uint32_t interface, Ipv4InterfaceAddress address);
+  virtual void PrintRoutingTable(Ptr<OutputStreamWrapper> stream, Time::Unit uint) const;
+
+
+  //>>>??
+  virtual void SetIpv4(Ptr<Ipv4> ipv4);
+  void SetPathUpdateInterval(Time pathUpdateInterval);
+  void SetLatestPathUtilUpdateTime(Time latestUpdateTime);
+
+  //processing probe packet && linkutil measurement:
+  uint32_t UpdateOutputTraffic(const Ipv4Header header&, Ptr<Packet> p, uint32_t port);
+  void UpdateOutputTrafficEvent(); 
+
+  //route 
+  //1. if probe packet
+  //  a). udpate hulaPathUtilTable and hulaBestNextHopTable
+  //2. if normal packet:
+  //  a). flowlet detect and route
+  Ptr<Ipv4Route> RouteOutput(Ptr<Packet> p, const Ipv4Header &header, Ptr<NetDevice> oif, Socket::SocketErrno &SocketErrno);
+  bool RouteInput(Ptr<const Packet> p, const Ipv4Header &header, Ptr<const NetDevice> idev,
+                            UnicastForwardCallback ucb, MulticastForwardCallback mcb, LocalDeliverCallback lcb,
+                            ErrorCallback ecb);
+
+
 
 private:
-  std::map<uint32_t, std::pair<uint32_t, double>> m_hulaPathUtilTable;  // <destTorId, <nextHop, pathUtil>>
-  std::map<uint32_t, uigithubnt32_t> m_hulaBestPathTable;   //<flowletId, nextHop>
+  Ptr<Ipv4> m_ipv4;
+  Time m_flowletTimeOut;
 
-  struct SwitchLayer{
-    uint32_t Tor;
-    uint32_t Agg;
-    uint32_t Core;
-  };
+  //processing probe packet && linkutil measurement:
+  Time m_pathUpdateIterval;   // link util update cycle
+  Time m_latestPathUtilUpdateTime;  // time between lastest pathUtil update. 
+  std::map<uint32_t, std::pair<Time, uint32_t>> m_outputLinkUtilMap; //<interface, <udpateTime, outputlinkUtil>>
+  std::map<uint32_t, uint32_t> m_outputTrafficMap;  //<interface, outputTrafficRegister>
+  
+  //pathutil statistics 
+  std::map<uint32_t, std::pair<uint32_t, uint32_t>> m_hulaPathUtilTable;  // <destTorId, <nextHop, pathUtil>>
 
+  //bestnexthop table
+  std::map<uint32_t, uint32_t> m_hulaBestPathTable;   //<flowletId, nextHop>
 };
 
 }
-
 #endif /* HULA_ROUTING_H */
 
